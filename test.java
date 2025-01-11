7d1e61f7b5a071918b550161b39dad83aaa2fb4d
YacineBoukliff <yacine69800@live.fr>
2025-01-11 23:04:18 +0100

    Operateur ternaire et les méthodes des string a maitriser

--- test.java
+++ test.java
@@ -4,28 +4,33 @@ class test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double prix = 10;
-        boolean vieux = true;
-        boolean etudiant = true;
+        String nom = "Yacine le jefe";
+        String prenom = nom.substring(0,6);
+        System.out.println(prenom);
+        // Trouver la longueur de la string
+        System.out.println(nom.length());
+        // Trouver la valeur de d'un caractère
+        System.out.println(nom.charAt(0));
+        // Trouver le premier endroit ou y'a le caractère
+        System.out.println(nom.indexOf("a"));
+        // Trouver le dernier endroit ou y'a le caractère
+        System.out.println(nom.lastIndexOf("a"));
+        // Mettre tout en minuscule
+        System.out.println(nom.toLowerCase());
+        // Mettre tout en majuscule
+        System.out.println(nom.toUpperCase());
+        // Mettre tout en minuscule
+        System.out.println(nom.trim());
 
-        if (etudiant && vieux) {
-        
-            System.out.println("Tu es étudiant et âgé, tu as droit à 30% de réduction.");
-            prix = prix * 0.7;
-        } else if (etudiant) {
-        
-            System.out.println("Tu es étudiant, tu as droit à 20% de réduction.");
-            prix = prix * 0.8;
-        } else if (vieux) {
-           
-            System.out.println("Tu es âgé, tu as droit à 10% de réduction.");
-            prix = prix * 0.9;
-        } else {
-          
-            System.out.println("Tu n'as droit à aucune réduction.");
-        }
 
-        System.out.println("Le prix final est : " + prix);
+        String palworld = "Palworld";
+
+       
+
+        String promo = (palworld.length() == 8) ? "C'est égal a 8" : "c'est pas égal a 5";
+
+        System.out.println(promo);
+
 
         scanner.close();
     }
