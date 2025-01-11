a44ec6b5d9c5f426ab70731825a11ef6bcf8fd77
YacineBoukliff <yacine69800@live.fr>
2025-01-11 15:13:06 +0100

    Variables

--- test.java
+++ test.java
@@ -1,6 +1,18 @@
 class test {
-    public static void main (String[] args){
-        System.out.println("Début Sur Java");
-        System.out.println("Tutoriel");
+    public static void main(String[] args) {
+
+        double age = 2.5;
+        int ageentier = 35;
+        char grade = 'A';
+        boolean jeSuisMajeur = true;
+
+        if(jeSuisMajeur) {
+            System.out.println("Tu es majeur");
+        }
+
+        System.out.println(age);
+        System.out.println(ageentier);
+        System.out.println(grade);
+        System.out.println(jeSuisMajeur);
     }
 }
