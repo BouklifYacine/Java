83db44d7fc482eded74ebcfe3e3d16157ef9d505
YacineBoukliff <yacine69800@live.fr>
2025-01-11 22:03:04 +0100

    Nested IF

--- test.java
+++ test.java
@@ -1,15 +1,34 @@
 import java.util.Scanner;
-import java.util.Random;
+
 
 class test {
     public static void main(String[] args) {
 
-        Random random = new Random();
+   Scanner scanner = new Scanner(System.in);
 
-        int nombre = random.nextInt(1,11);
+   double prix = 10; 
+   boolean vieux = true;
+   boolean etudiant = true;
 
-        System.out.println(nombre);
-      
+   if(etudiant){
+  
+    System.out.println("Tu es étudiant tu as le droit a 20% de réduc");
+    if (vieux) {
+        System.out.println("Tu es vieux et étudiant tu as le droit a 30% de reduc");
+        prix = prix * 0.7;
+        System.out.println(prix);
+    }
+   }
+   else if (vieux){
+System.out.println("Tu es vieux tu as 10% de reduc");
+prix = prix * 0.9;
+System.out.println(prix);
+   }
+   else {
+    prix = prix * 1;
+    System.out.println(prix);
+   }
 
-
+       
+      scanner.close();
 }}
